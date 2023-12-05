a9aa6f32fa5a75382dd7c9b0a26c3e8bb2533f24
Naweed Yarzada <yarzadnx@dukes.jmu.edu>
2023-12-05 14:29:06 -0500

    did nothing

--- forum_testing/data.js
+++ forum_testing/data.js
@@ -1,48 +1,48 @@
 var threads = [
-    {
-        id: 1,
-        title: "Thread 1",
+  {
+    id: 1,
+    title: "Thread 1",
+    author: "Aaron",
+    data: Date.now(),
+    content: "Thread content",
+    comments: [
+      {
+        author: "Jack",
+        date: Date.now(),
+        content: "Hey there",
+      },
+      {
         author: "Aaron",
-        data: Date.now(),
-        content: "Thread content",
-        comments: [
-            {
-                author: "Jack",
-                date: Date.now(),
-                content: "Hey there"
-            },
-            {
-                author: "Aaron",
-                date: Date.now(),
-                content: "Hey there too"
-            }
-        ]
-    },
-    {
-        id: 2,
-        title: "Thread 2",
+        date: Date.now(),
+        content: "Hey there too",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Thread 2",
+    author: "Aaron",
+    data: Date.now(),
+    content: "Thread content",
+    comments: [
+      {
+        author: "Jack",
+        date: Date.now(),
+        content: "Hey there",
+      },
+      {
         author: "Aaron",
-        data: Date.now(),
-        content: "Thread content",
-        comments: [
-            {
-                author: "Jack",
-                date: Date.now(),
-                content: "Hey there"
-            },
-            {
-                author: "Aaron",
-                date: Date.now(),
-                content: "Hey there too"
-            }
-        ]
-    }
-]
+        date: Date.now(),
+        content: "Hey there too",
+      },
+    ],
+  },
+];
 
 var threads;
-if (localStorage && localStorage.getItem('threads')){
-    threads = JSON.parse(localStorage.getItem('threads'));
+if (localStorage && localStorage.getItem("threads")) {
+  threads = JSON.parse(localStorage.getItem("threads"));
 } else {
-    threads = defaultThreads;
-    localStorage.setItem('threads', JSON.stringify(defaultThreads));
+  threads = defaultThreads;
+  localStorage.setItem("threads", JSON.stringify(defaultThreads));
 }
